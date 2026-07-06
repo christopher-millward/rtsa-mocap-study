7fd6c7e783d67b55c681aa658bf33a4db7c1e8a1
cmillward13 <chris.millward13@hotmail.com>
2026-07-06 16:27:16 -0400

    - Updated determinance tolerance.

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw_normalized_data"
 
 ORTHONORMAL_TOLERANCE = 5e-4
-DETERMINANT_TOLERANCE = 1e-6
+DETERMINANT_TOLERANCE = 5e-4
 
 RESULTS_PATH = PROJ_ROOT / "outputs" / "results.csv"
