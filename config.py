c78be3f076a568f7150349d38e25b12ce65b5ffc
cmillward13 <chris.millward13@hotmail.com>
2026-07-06 15:13:28 -0400

    - Added extensions to resultspath

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ RAW_DATA_DIR = DATA_DIR / "raw_normalized_data"
 ORTHONORMAL_TOLERANCE = 1e-4
 DETERMINANT_TOLERANCE = 1e-6
 
-RESULTS_PATH = PROJ_ROOT / "outputs" / "results"
+RESULTS_PATH = PROJ_ROOT / "outputs" / "results.csv"
