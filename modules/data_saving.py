bb4532ce2ff216f251ae7d2ce3b52e2be14e5622
cmillward13 <chris.millward13@hotmail.com>
2026-07-31 11:25:19 -0400

    - Modified docstring for save_heatmap_to_csv.

--- modules/data_saving.py
+++ modules/data_saving.py
@@ -16,12 +16,13 @@ def save_data_to_pickle(data, file_path):
         pickle.dump(data, f)
 
 
-def save_heatmap_to_csv(all_heatmap_data, file_path):
+def save_heatmap_to_csv(all_data, file_path):
     """Saves the heatmap data for all participants into a single CSV file.
 
     The CSV will hav the following columns:
         - fname: The filename of the participant's data.
         - side: The side of the body (left or right).
+        - motion_type: The type of motion (humerothoracic or glenohumeral).
         - unit: The unit of measurement for the heatmap data.
         - bin_elevation_start: The starting elevation angle for the heatmap.
         - bin_elevation_end: The ending elevation angle for the heatmap.
@@ -35,8 +36,8 @@ def save_heatmap_to_csv(all_heatmap_data, file_path):
 
 
     Args:
-        all_heatmap_data: The heatmap data to be saved (should be in a format compatible with CSV).
-        file_path: The path to the CSV file where the heatmap data will be saved.
+        all_data(List[ParticipantDetails]): The final data object containing all participant details and their corresponding heatmap data.
+        file_path: The path to the CSV file where the participant data will be saved.
 
     Returns:
         None
