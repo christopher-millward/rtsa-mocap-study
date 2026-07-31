86ed305db470c241ee587362f2f91dff3d0a41fd
cmillward13 <chris.millward13@hotmail.com>
2026-07-31 10:04:53 -0400

    - Organized into groups. - Added output paths.

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
 from pathlib import Path
-
+# Directories
 PROJ_ROOT = Path(__file__).resolve().parents[0]
-
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw_normalized_data"
+OUTPUTS_DIR = PROJ_ROOT / "outputs"
 
+# Specific files
+RESULTS_PICKLE_PATH = OUTPUTS_DIR / "all_results.pkl"
+RESULTS_HEATMAP_CSV_PATH = OUTPUTS_DIR / "heatmap_results_data.csv"
+
+# Analysis and Testing Parameters
 ORTHONORMAL_TOLERANCE = 5e-4
 DETERMINANT_TOLERANCE = 5e-4
 SMALLEST_CLINICALLY_RELEVANT_ANGLE = 1e-3
 TEST_PRECISION_TOLERANCE = 1e-9
 TEST_SINGULARITY_TOLERANCE = 1e-7
 
-RESULTS_PATH = PROJ_ROOT / "outputs" / "results.csv"
