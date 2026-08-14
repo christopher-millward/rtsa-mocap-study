8782bf9d6f8a6f627129ca9e57adafadc12bf141
cmillward13 <chris.millward13@hotmail.com>
2026-08-14 15:30:52 -0400

    Added to docstring.

--- schema.py
+++ schema.py
@@ -13,9 +13,10 @@ class Heatmap:
     """
     Type definition for a heatmap.
 
-    Each of elevation, poe, and ir_er is a 2D array representing the cumulative 
-    motion in each bin (grid cell) of the heatmap. The shape of these arrays is
-    determined by the bin_width, which must evenly divide 360 degrees.
+    Each of elevation, poe, and ir_er is a 2D array (shape n_elev_bins, n_poe_bins) 
+    representing the cumulative motion in each bin (grid cell) of the heatmap. 
+    The shape of these arrays is determined by the bin_width, which must evenly 
+    divide 360 degrees.
 
     Attributes:
         bin_width (int): Width of each heatmap bin in degrees.
